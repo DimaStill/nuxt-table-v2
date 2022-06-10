886ea87788326d74af8b5b108960b0af2d31e778
Dmytro Karaiev <dmytro.karaiev@svitsoft.com>
2022-06-10 14:37:23 +0300

    Create test project for revogrid

---
+++ components/Tutorial.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <button @click="save">Save</button>
+    <v-grid theme="compact" :source="rows" :columns="columns" @aftercolumnsset="debug"></v-grid>
+    <div v-if="isSaved">
+      <p>What changed?</p>
+      <p>{{ JSON.stringify(rows) }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import VGrid from "@revolist/vue-datagrid";
+import { fakeCols, fakeRows } from '../generateFakeData.js';
+export default {
+  name: "App",
+  data() {
+    return {
+      columns: fakeCols,
+      rows: fakeRows,
+      isSaved: false
+    };
+  },
+  methods: {
+    debug(e) {
+      console.log('DEBUG:', e);
+    },
+    save() {
+      this.isSaved = true;
+    }
+  },
+  components: {
+    VGrid,
+  },
+};
+</script>
+
+<style>
+#app {
+  height: 100vh;
+  width: 100vh;
+}
+revo-grid {
+  height: 50%;
+}
+</style>
---
+++ generateFakeData.js
@@ -0,0 +1,80 @@
+export const fakeCols = [
+  {
+    labelKey: 'label',
+    valueKey: 'value',
+    source: [
+        { label: 'According', value: 'a' },
+        { label: 'Over', value: 'b' },
+        { label: 'Source', value: 's' }
+    ],
+    prop: "name",
+    name: "userName",
+    columnType: 'select'
+  },
+  {
+    prop: "details",
+    name: "userDetails",
+    columnType: 'string'
+  },
+  {
+    prop: "Surname",
+    name: "userSurname",
+    columnType: 'string'
+  },
+  {
+    prop: "Age",
+    name: "userAge",
+    columnType: 'numeric'
+  },
+  {
+    prop: "car",
+    name: "Car"
+  },
+  {
+    prop: "animal",
+    name: "Animal"
+  },
+];
+
+export const fakeRows = [
+  {
+    name: "John",
+    details: "Item 1",
+    Surname: "Mysurname",
+    Age: "25",
+    car: 'Truck',
+    animal: 'None'
+  },
+  {
+    name: "Jack",
+    details: "Item 2",
+    Surname: "Surname",
+    Age: "40",
+    car: 'None',
+    animal: 'Cat'
+  },
+  {
+    name: "Dima",
+    details: "Item 3",
+    Surname: "Jackson",
+    Age: "23",
+    car: 'None',
+    animal: 'Dog'
+  },
+  {
+    name: "Anna",
+    details: "Item 4",
+    Surname: "Smith",
+    Age: "18",
+    car: 'None',
+    animal: 'Parrot'
+  },
+  {
+    name: "2",
+    details: "Item 5",
+    Surname: "Jones",
+    Age: "37",
+    car: 'None',
+    animal: 'Fishes'
+  },
+]
